a8110847230169d5eda40e32f2a7db4079dc86bc
Cesar Welchez <cesar777welch@gmail.com>
2021-12-10 17:53:15 -0300

    whole day

--- src/Component/Heatmap.js
+++ src/Component/Heatmap.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import h337 from "heatmapjs";
 import Slider from "@mui/material/Slider";
 import Image from "../assets/itcPlan.png";
+import { data as ogData } from "../assets/heatmapdata";
 import { data } from "../assets/formattedheatmapdata";
 import "./styles.css";
 
@@ -10,15 +11,15 @@ export default function Heatmap() {
   // const [pointsInfo, setPointsInfo] = useState(data[0]);
   const [pointsInfo, setPointsInfo] = useState([data[0]]);
 
-  // const test = data.map((i) =>
-  //   i.map((j) => {
-  //     return { x: j.x / 4, y: j.y / 4, value: (j.value / 100) * 4 };
-  //   })
-  // );
-  // console.log(
-  //   "🚀 ~ file: Heatmap.js ~ line 18 ~ Heatmap ~ test",
-  //   JSON.stringify(test)
-  // );
+  const test = ogData.map((i) =>
+    i.map((j) => {
+      return { x: j.x / 4, y: j.y / 4, value: (j.value / 100) * 4 };
+    })
+  );
+  console.log(
+    "🚀 ~ file: Heatmap.js ~ line 18 ~ Heatmap ~ test",
+    JSON.stringify(test)
+  );
 
   useEffect(() => {
     const instance = h337.create({
@@ -59,7 +60,7 @@ export default function Heatmap() {
     <>
       <div style={{ width: "400px" }}>
         <Slider
-          max={61}
+          max={541}
           defaultValue={0}
           aria-label="Default"
           valueLabelDisplay="auto"
@@ -71,7 +72,7 @@ export default function Heatmap() {
         <div id="mydiv" className="heatmap" style={{ display: "block" }}>
           <img height="703px" width="810px" src={Image} alt="road" />
         </div>
-        <div style={{ textAlign: "left", paddingLeft:"2rem" }}>
+        <div style={{ textAlign: "left", paddingLeft: "2rem" }}>
           <div>blue: 0 - 200</div>
           <div>cyan: 200 - 500</div>
           <div>yellow: 500 - 1000</div>
